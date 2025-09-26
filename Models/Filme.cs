e4265102db4ecb8aba47731af2b51ea3a4efd90e
Artasi67 <athurtavaressilva@gmail.com>
2025-09-26 08:36:49 -0300

    quase nada ainda

---
+++ Models/Filme.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ArthurTavares.Models
+{
+    public class Filme
+    {
+        public int Id_filme { get; set; }
+        public string Titulo_filme { get; set; }
+        public string Descricao_filme { get; set; }
+        public DateOnly Lancamento_filme { get; set; }
+
+
+        public int Id_usuario { get; set; }
+        [ForeignKey("Id_usuario")]
+        public Usuario Usuario { get; set; }
+    }
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ArthurTavares.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int Id_usuario { get; set; }
+        public string Nome_usuario { get; set; }
+        public string Email_usuario { get; set; }
+        public string Senha_usuario { get; set; }
+    }
+}
