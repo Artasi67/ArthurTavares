a0cc1af9a3dc743e6825f11c12a665fb6acb9185
Artasi67 <athurtavaressilva@gmail.com>
2025-09-26 08:48:53 -0300

    dbconfig ta no talo

---
+++ Config/DbConfig.cs
@@ -0,0 +1,13 @@
+﻿using ArthurTavares.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArthurTavares.Config
+{
+    public class DbConfig : DbContext
+    {
+        public DbConfig(DbContextOptions<DbConfig> options) : base(options) { }
+
+        public DbSet<Usuario> usuarios { get; set; }
+        public DbSet<Filme> filmes { get; set; }
+    }
+}
