98210d45b5feec993ddb3e4c5790c21d3cf689ad
Artasi67 <athurtavaressilva@gmail.com>
2025-10-03 08:23:49 -0300

    falta pouco dmss

--- Program.cs
+++ Program.cs
@@ -1,7 +1,26 @@
+using ArthurTavares.Config;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddAuthentication("CookieAuthentication")
+    .AddCookie("CookieAuthentication", config =>
+    {
+        config.Cookie.Name = "UsuarioLoginCookie";
+        config.LoginPath = "/Autenticacao/Login";
+        config.AccessDeniedPath = "/Autenticacao/Login";
+    });
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<DbConfig>(options =>
+options.UseMySql(
+    builder.Configuration.GetConnectionString("DefaultConnection"),
+    ServerVersion.AutoDetect(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+        )
+    )
+);
 
 var app = builder.Build();
 
@@ -14,10 +33,12 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
+
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Autenticacao}/{action=Login}/{id?}");
 
 app.Run();
